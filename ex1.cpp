9c43e6ec57fe9f2c73f4a2465910017a6828aba3
Albin Ho <albin_ho@yahoo.co.uk>
2025-03-14 14:11:31 +0000

    merged stash changes

--- ex1.cpp
+++ ex1.cpp
@@ -18,23 +18,26 @@ int main() {
   int rows = 4;
   int cols = 4;
   int double_array[rows][cols];
+
   for (int i = 0; i < 4; i++) {
     for (int j = 0; j < 4; j++) {
-      int count = j;
+      int count = j + 1;
       cout << "Inner loop, Enter:" << count << "\t";
       cin >> double_array[i][j];
       count++;
     }
-    cout << "\n";
-    cout << "outerloop" << i << endl;
+    int count2 = i + 1;
+    cout << "outerloop " << count2 << "\n";
+    // cout << "\n";
+    count2++;
   }
 
   cout << "The numbers you typed as follows: " << "\n";
   for (int i = 0; i < 4; i++) {
     for (int j = 0; j < 4; j++) {
-      int count = j;
+      // int count = j;
       cout << double_array[i][j] << "\t";
-      count++;
+      // count++;
     }
     // cout << double_array[i] << "\t";
     cout << "\n";
