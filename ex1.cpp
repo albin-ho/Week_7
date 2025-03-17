b0208c0359e5013d96553360ea991e7e16367836
Albin Ho <albin_ho@yahoo.co.uk>
2025-03-17 12:13:24 +0000

    Finished program, counter sill resets to 1 4 inputs

--- ex1.cpp
+++ ex1.cpp
@@ -18,38 +18,38 @@ int main() {
   int rows = 4; // use row/cols vars for size of array
   int cols = 4;
   int double_array[rows][cols]; // initialise array
-  // start of for loop
+  int total_input;
+  cout << "This program demonstrates a 2D array" << "\n";
+  cout << "The array has " << rows << " rows and " << cols << " columns"
+       << "\n";
+  total_input = rows * cols;
+  cout << "Please enter " << total_input << " numbers below:" << "\n";
+
+  // start input for loop
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 4; j++) { // nested for loop
+      int count = j + 1;
+      cout << "Enter:" << count << "\t";
+      cin >> double_array[i][j];
+      count++;
+    }
+    // int count2 = 1;
+    // cout << "outerloop " << count2 << "\n";
+    // count2++;
+    //  cout << "outerloop" << i << endl;
+    cout << "\n";
+  }
+
+  cout << "The numbers you typed as follows: " << "\n";
   for (int i = 0; i < 4; i++) {
     for (int j = 0; j < 4; j++) {
-      int rows = 4;                 // set size of array using variables
-      int cols = 4;                 // set size of array using variables
-      int double_array[rows][cols]; // initialize array
-                                    // start input for loop
-      for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) { // nested for loop
-          int count = j + 1;
-          cout << "Inner loop, Enter:" << count << "\t";
-          cin >> double_array[i][j];
-          count++;
-        }
-        int count2 = 1;
-        cout << "outerloop " << count2 << "\n";
-        count2++;
-        // cout << "\t";
-        //  cout << "outerloop" << i << endl;
-      }
-
-      cout << "The numbers you typed as follows: " << "\n";
-      for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
-          // int count = j;
-          cout << double_array[i][j] << "\t";
-          // count++;
-        }
-        // cout << double_array[i] << "\t";
-        cout << "\n";
-        //    cout << "\t";
-      }
-
-      return 0;
+      // int count = j;
+      cout << double_array[i][j] << "\t";
+      // count++;
     }
+    // cout << double_array[i] << "\t";
+    cout << "\n";
+    //    cout << "\t";
+  }
+  return 0;
+}
