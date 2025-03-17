cf8a0b7edbd1fd50770c00efebc3bbbe9ed387ee
Albin Ho <albin_ho@yahoo.co.uk>
2025-03-17 17:06:18 +0000

    program outputs array, but bad formatting

---
+++ ex4.cpp
@@ -0,0 +1,30 @@
+// Program Purpose:ex4, Program to show square root array
+// Author:
+// Last Modified:17/03/2025
+//
+//**********************************************************************************
+// #include <cmath>
+// #include <cstdlib>
+#include <iostream>
+// #include <string>
+// #include <vector>
+// #include <iomanip>
+// #include <ctime>
+// #include <fstream>
+// #include <iostream>
+using namespace std;
+
+int main() {
+  int squared_array[][2] = {1, 1,  2, 4,  3, 9,  4, 16, 5,  25,
+                            6, 36, 7, 49, 8, 64, 9, 81, 10, 100};
+
+  cout << "This program uses an array to calculate square roots " << "\n";
+  for (int i = 0; i < 9; i++) {
+    for (int j = 0; j < 9; j++) {
+      cout << squared_array[i][j] << "\t";
+    }
+    cout << "\n";
+  }
+  //  cout << "the square root of 6: " << squared_array[5][1] << "\n";
+  return 0;
+}
