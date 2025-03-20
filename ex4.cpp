3b87633c6a9df1d7d27b18c925e9e15973a40527
Albin Ho <albin_ho@yahoo.co.uk>
2025-03-20 14:10:18 +0000

    Finished Program

--- ex4.cpp
+++ ex4.cpp
@@ -8,7 +8,7 @@
 #include <iostream>
 // #include <string>
 // #include <vector>
-// #include <iomanip>
+#include <iomanip>
 // #include <ctime>
 // #include <fstream>
 // #include <iostream>
@@ -19,10 +19,14 @@ int main() {
                             6, 36, 7, 49, 8, 64, 9, 81, 10, 100};
 
   cout << "This program uses an array to calculate square roots " << "\n";
-  for (int i = 0; i < 9; i++) {
-    for (int j = 0; j < 9; j++) {
-      cout << squared_array[i][j] << "\t";
+  int size = sizeof(squared_array) /
+             sizeof(squared_array[0]); // Calculete number of elements in array
+  cout << "The size of the array is: " << size << "\n";
+  for (int i = 0; i < 6; i++) {
+    for (int j = 0; j < size; j++) {
+      cout << "|" << squared_array[i][j] << "|" << "    ";
     }
+    // cout << "x" << "\n";
     cout << "\n";
   }
   //  cout << "the square root of 6: " << squared_array[5][1] << "\n";
