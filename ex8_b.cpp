ec835322149b1d2fdb54a80bed51509f3f9f0f7e
Albin Ho <albin_ho@yahoo.co.uk>
2025-04-22 15:44:54 +0100

    finished Program

---
+++ ex8_b.cpp
@@ -0,0 +1,27 @@
+// Program Purpose:ex8, example code explanation
+// Author:
+// Last Modified:
+//
+//**********************************************************************************
+// #include <cmath>
+// #include <cstdlib>
+#include <iostream>
+// #include <string>
+// #include <vector>
+// #include <iomanip>
+// #include <ctime>
+// #include <fstream>
+// #include <iostream>
+using namespace std;
+
+int main() {
+  int var1;
+
+  char var2[10];
+
+  cout << "something doing " << &var1 << endl;
+
+  cout << "something doing " << &var2 << endl;
+
+  return 0;
+}
